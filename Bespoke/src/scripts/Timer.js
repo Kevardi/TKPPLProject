8e610c4db3b733478fae8671fd6c0e299cf6beb4
Kevin <kevin.leonardi13@gmail.com>
2017-06-04 22:19:01 +0700

    Javascript Timer

---
+++ Bespoke/src/scripts/Timer.js
@@ -0,0 +1,27 @@
+﻿function timer()
+{
+    if (x >= 0)
+        return x--;
+
+    return "";
+}
+
+function timer2()
+{
+    var a = timer();
+
+    if (a >= 0 || a == "")
+        document.getElementById("timer").innerHTML = a;
+    else
+        clearInterval(intervalTimer);
+}
+
+function clearTimer()
+{
+    clearInterval(intervalTimer);
+}
+
+var x = 30;
+var intervalTimer = setInterval(timer2, 1000);
+
+
